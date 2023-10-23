22d3c2795b4fbc377f2c63c44632c1608288cd77
Luanshow <117780146+Luanshow@users.noreply.github.com>
2023-10-23 18:37:02 -0300

    Initial commit

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,22 @@
+import { NgModule, importProvidersFrom } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+import { ActionSheetComponent } from '../componets/action-sheet/action-sheet.component';
+
+import { HomePageRoutingModule } from './home-routing.module';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule
+  ],
+  declarations: [HomePage, ActionSheetComponent]
+
+
+})
+export class HomePageModule {}
